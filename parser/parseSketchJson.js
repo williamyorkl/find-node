032167c8ebc90e6ed40d133cbd2b512f9441056d
williamyorkl <apple@appledeMacBook-Air.local>
2021-10-02 09:57:53 +0800

    添加测试对比资源

---
+++ parser/parseSketchJson.js
@@ -0,0 +1,17 @@
+import sketchJson from "../raw-source/test-unit1.json";
+
+/**
+ *
+ * page
+ *  - 'XXX - XXX - XXX '
+ *    - frame
+ *    - style
+ *    - layer: Array<{}>
+ *      - artboard:{}
+ *        - layer: Array<{}>
+ *          -  header-container:{}
+ *          -  body-container:{}
+ *          -  footer-container:{}
+ *
+ *
+ */
--- test-unit1/src/App.vue
+++ test-unit1/src/App.vue
@@ -15,4 +15,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+body {
+  margin: 0;
+}
+</style>
